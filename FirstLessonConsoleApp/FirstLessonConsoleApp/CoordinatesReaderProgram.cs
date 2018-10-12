01164ab4b9922e28b39d0017825f3cd7448fc77e
Aliaksandr Makarchuk <Aliaksandr_Makarchuk@epam.com>
2018-10-12 10:18:34 +0300

    Refactoring

---
+++ FirstLessonConsoleApp/FirstLessonConsoleApp/CoordinatesReaderProgram.cs
@@ -0,0 +1,80 @@
+﻿using FirstLessonConsoleApp.Menu;
+using FirstLessonConsoleApp.View;
+using System.Collections.Generic;
+
+namespace FirstLessonConsoleApp
+{
+    public class CoordinatesReaderProgram
+    {
+        private IView _view;
+        private ICollection<MenuItem> _menuItems = new List<MenuItem>();
+
+        public CoordinatesReaderProgram(IView view, string coordinatesFilePath)
+        {
+            _view = view;
+            InitializeMenuItems(coordinatesFilePath);
+        }
+
+        public void RunProgram()
+        {
+            while (true)
+            {
+                _view.ClearScreen();
+                _view.PrintMenuItems(_menuItems);
+
+                var userInput = _view.PromptInput("Введите номер элемента меню");
+                var menuItem = TryGetMenuItem(userInput);
+
+                ExecuteMenuItem(menuItem);
+            }
+        }
+
+        private void ExecuteMenuItem(MenuItem menuItem)
+        {
+            if (menuItem == null)
+            {
+                _view.PrintMessage("Введите правильный номер элемента меню");
+                return;
+            }
+
+            _view.ClearScreen();
+            menuItem.Execute();
+            _view.PressAnyKeyToContinue();
+        }
+
+        private void InitializeMenuItems(string coordinatesFilePath)
+        {
+            _menuItems.Add(new ReadCoordinatesFromConsoleMenuItem(1, "ввод координат с клавиатуры", _view));
+            _menuItems.Add(new ReadCoordinatesFromFileMenuItem(2, "ввод координат из файла", _view, coordinatesFilePath));
+            _menuItems.Add(new ExitMenuItem(3, "выход", _view));
+        }
+
+        private MenuItem TryGetMenuItem(string userInput)
+        {
+            var index = 0;
+
+            if (!int.TryParse(userInput, out index))
+            {
+                return null;
+            }
+
+            return FindMenuItemByIndex(index);
+        }
+
+        private MenuItem FindMenuItemByIndex(int index)
+        {
+            MenuItem selectedItem = null;
+
+            foreach (var item in _menuItems)
+            {
+                if (item.OrderNumber == index)
+                {
+                    selectedItem = item;
+                    break;
+                }
+            }
+
+            return selectedItem;
+        }
+    }
+}
---
+++ FirstLessonConsoleApp/FirstLessonConsoleApp/Menu/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using FirstLessonConsoleApp.View;
+
+namespace FirstLessonConsoleApp.Menu
+{
+    public abstract class MenuItem
+    {
+        private IView _view;
+        public int OrderNumber { get; }
+        public string Text { get; }
+
+        public MenuItem(int orderNumber, string text, IView view)
+        {
+            OrderNumber = orderNumber;
+            Text = text;
+            _view = view;
+        }
+
+        protected IView GetView()
+        {
+            return _view;
+        }
+
+        public abstract void Execute();
+    }
+}
