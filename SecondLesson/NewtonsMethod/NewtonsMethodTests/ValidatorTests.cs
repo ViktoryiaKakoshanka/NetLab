8e808f43f158c62d080962477600d05e7fb25455
Viktoryia <Viktoryia_Kakoshanka@epam.com>
2018-11-13 17:12:13 +0300

    UnitTests for Validator - lesson 2

--- SecondLesson/NewtonsMethod/NewtonsMethodTests/ValidatorTests.cs
+++ SecondLesson/NewtonsMethod/NewtonsMethodTests/ValidatorTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NewtonsMethod.Controller;
 using NewtonsMethod.Model;
 
@@ -12,12 +11,40 @@ namespace NewtonsMethodTests
         public void ValidateInputPowerTest()
         {
             bool actual;
-            var inputString = new[] { "1.0", "1", "56565", "df", "5fe", "" };
-            var exected = new[] { false, true, true, false, false, false };
+            var inputString = new[] { "1.0", "1", "56565", "df", "5fe", "", "1,54" };
+            var exected = new[] { false, true, true, false, false, false, false };
 
             for(var i = 0; i > inputString.Length; i++)
             {
                 actual = Validator.ValidateInput(inputString[i], InputedParams.Power);
+                Assert.AreEqual(exected[i], actual);
+            }
+        }
+        
+        [TestMethod]
+        public void ValidateInputNumericalTest()
+        {
+            bool actual;
+            var inputString = new[] { "1.0", "1", "56565", "df", "5fe", "", "1,54" };
+            var exected = new[] { true, true, true, false, false, false, true };
+
+            for (var i = 0; i > inputString.Length; i++)
+            {
+                actual = Validator.ValidateInput(inputString[i], InputedParams.Numerical);
+                Assert.AreEqual(exected[i], actual);
+            }
+        }
+
+        [TestMethod]
+        public void ValidateInputАccurancyTest()
+        {
+            bool actual;
+            var inputString = new[] { "1.0", "1", "56565", "df", "5fe", "", "1,54" };
+            var exected = new[] { true, true, true, false, false, false, true };
+
+            for (var i = 0; i > inputString.Length; i++)
+            {
+                actual = Validator.ValidateInput(inputString[i], InputedParams.Аccurancy);
                 Assert.AreEqual(exected[i], actual);
             }
         }
