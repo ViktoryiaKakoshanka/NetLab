6ed5c148ac54105a042a255a970ae257a7d6d54c
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-01-04 14:27:42 +0300

    Renovetion WorkWithFile

--- WorkWithFiles/WorkWithFiles/FileService.cs
+++ WorkWithFiles/WorkWithFiles/FileService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkWithFiles
 {
-    class FileService
+    public class FileService
     {
+        public string Open(string filePath)
+        {
+            byte[] text;
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                fileStream.Read(text = new byte[(int)fileStream.Length], 0, (int)fileStream.Length);
+            }
+            return Encoding.UTF8.GetString(text);
+        }
+
+        public void Save(string filePath, string text)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                fileStream.Write(Encoding.ASCII.GetBytes(text), 0, text.Length);
+            }
+        }
     }
 }
--- WorkWithFiles/WorkWithFiles/RelayCommand.cs
+++ WorkWithFiles/WorkWithFiles/RelayCommand.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WorkWithFiles
 {
-    class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
