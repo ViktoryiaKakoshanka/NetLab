450fa2a44b0b9c72d09a6e2956dd62a3678ff8b9
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-01-04 18:07:07 +0300

    Renovetion FirstLessons

---
+++ FirstLessonWPFApplication/FirstLessonWPFApplication/Controller/CoordinatesFromFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace FirstLessonWPFApplication.Controller
+{
+    class CoordinatesFromFile
+    {
+        private List<string> listCoordinatesFromFile = new List<string>();
+
+        public void ReadFileInListbox(string pathFile)
+        {
+            using (StreamReader instanceStreamReader = new StreamReader(pathFile, Encoding.Default))
+            {
+                string coordinatePair;
+                while ((coordinatePair = instanceStreamReader.ReadLine()) != null)
+                {
+                    listCoordinatesFromFile.Add(coordinatePair);
+                }
+            }
+        }
+
+        public List<string> GetListCoordinatesFromFile()
+        {
+            return listCoordinatesFromFile;
+        }
+    }
+}
