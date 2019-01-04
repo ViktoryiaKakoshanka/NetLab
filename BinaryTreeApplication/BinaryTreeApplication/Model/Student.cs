b56a4b8a12193ed035622fca3523461418d5131c
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-01-04 15:33:23 +0300

    Renovetion BinaryTree

---
+++ BinaryTreeApplication/BinaryTreeApplication/Model/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace BinaryTreeApplication.Model
+{
+    [Serializable]
+    public class Student : IComparer<Student>
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+
+        public Student(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Compare(Student x, Student y)
+        {
+           return CultureInfo.CurrentCulture.CompareInfo.Compare(x?.Name, y?.Name, CompareOptions.None);
+        }
+    }
+}
---
+++ BinaryTreeApplication/BinaryTreeApplication/Model/StudentTestRegister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreeApplication.Model
+{
+    [Serializable]
+    public class StudentTestRegister : IComparer<StudentTestRegister>
+    {
+        public Test TestPassed { get; }
+        public Student Student { get; }
+        public DateTime Date { get; }
+        public int Mark { get; }
+
+        public StudentTestRegister(Test testPassed, Student student, DateTime date, int mark)
+        {
+            TestPassed = testPassed;
+            Student = student;
+            Date = date;
+            Mark = mark;
+        }
+
+        public int Compare(StudentTestRegister x, StudentTestRegister y)
+        {
+            if (x == null || y == null)
+            {
+                throw new NullReferenceException();
+            }
+            return DateTime.Compare(x.Date, y.Date);
+        }
+    }
+}
