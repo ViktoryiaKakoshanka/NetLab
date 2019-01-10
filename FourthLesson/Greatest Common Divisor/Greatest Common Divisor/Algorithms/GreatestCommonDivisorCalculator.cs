7d19e50c7ad6c2f1175480473b5fec6bcffc3c4f
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-10 09:54:52 +0300

    Refactoring GCD

---
+++ FourthLesson/Greatest Common Divisor/Greatest Common Divisor/Algorithms/GreatestCommonDivisorCalculator.cs
@@ -0,0 +1,18 @@
+﻿using Greatest_Common_Divisor.Model;
+
+namespace Greatest_Common_Divisor.Algorithms
+{
+    public class AlgorithmHelper
+    {
+        public static GcdResult Calculate(int[] numbers, AlgorithmType algorithmType)
+        {
+            var algorithm = GetAlgorithm(algorithmType);
+            return algorithm.Calculate(numbers);
+        }
+
+        private static IAlgorithm GetAlgorithm(AlgorithmType algorithmType)
+        {
+            return new AlgorithmFactory().CreateAlgorithm(algorithmType);
+        }
+    }
+}
