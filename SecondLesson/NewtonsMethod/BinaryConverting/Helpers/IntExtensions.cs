49d2c2ac5bba720f52af302054e0fb4bbd92fb02
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-14 01:01:57 +0300

    Refactoring ConversionNumeric

---
+++ SecondLesson/NewtonsMethod/BinaryConverting/Helpers/IntExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace BinaryConverting.Helpers
+{
+    public static class IntExtensions
+    {
+        public static string ByConvert(this int number)
+        {
+            var binary = new StringBuilder();
+
+            while (number > 0)
+            {
+                var remainder = number % 2;
+                number /= 2;
+                binary.Append(remainder);
+            }
+            
+            return new string(binary.ToString().Reverse().ToArray());
+        }
+    }
+}
