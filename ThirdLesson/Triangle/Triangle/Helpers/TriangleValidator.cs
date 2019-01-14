ea674ecb5e992e73e1fee48fb5b9d551f8fb9eca
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-14 08:16:09 +0300

    Refactoring TriangleLib

---
+++ ThirdLesson/Triangle/Triangle/Helpers/TriangleValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace TriangleLib.Helpers
+{
+    public class TriangleValidator
+    {
+        public static bool Validate(double firstEdge, double secondEdge, double thirdEdge)
+        {
+            return firstEdge < secondEdge + thirdEdge && 
+                   secondEdge < firstEdge + thirdEdge && 
+                   thirdEdge < firstEdge + secondEdge;
+        }
+    }
+}
