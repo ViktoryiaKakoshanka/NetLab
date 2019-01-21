d6228318facfebc94fa36d5f0e54414365715089
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-21 12:06:05 +0300

    Refactoring first lesson

---
+++ FirstLessonWPFApplication/FirstLessonWPFApplication/FileHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FirstLessonWPFApplication
+{
+    internal static class CoordinatesFromFile
+    {
+        public static List<string> ReadFileInListbox(string pathFile)
+        {
+            var readLines = File.ReadAllLines(pathFile);
+            return readLines.ToList();
+        }
+    }
+}
---
+++ FirstLessonWPFApplication/FirstLessonWPFApplication/Formatter.cs
@@ -0,0 +1,13 @@
+﻿using FirstLessonConsoleApp.Model;
+
+namespace FirstLessonWPFApplication
+{
+    internal static class Formatter
+    {
+        public static string FormatCoordinates(string userInput)
+        {
+            var coordinate = CoordinatesHelper.ParseUserInputToCoordinate(userInput);
+            return coordinate != null ? coordinate.ToString() : string.Empty;
+        }
+    }
+}
