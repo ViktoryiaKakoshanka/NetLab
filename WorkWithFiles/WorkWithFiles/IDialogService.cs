157a2944538d3e3fb67d30f5be164eb4110db89b
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-21 14:59:59 +0300

    -

---
+++ WorkWithFiles/WorkWithFiles/IDialogService.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkWithFiles
+{
+    public interface IDialogService
+    {
+        string FilePath { get; set; }
+
+        bool ShowOpenFileDialog();
+        bool ShowSaveFileDialog();
+        void ShowMessage(string message);
+    }
+}
---
+++ WorkWithFiles/WorkWithFiles/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkWithFiles
+{
+    public interface IFileService
+    {
+        string Open(string filePath);
+        void Save(string filePath, string text);
+    }
+}
