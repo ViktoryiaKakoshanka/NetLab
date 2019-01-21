59c9aac01cef35df4ebeac796fa278aa0d499887
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-21 08:24:32 +0300

    Refactoring first lesson

---
+++ FirstLessonWPFApplication/FirstLessonWPFApplication/Format.cs
@@ -0,0 +1,13 @@
+﻿using FirstLessonConsoleApp.Model;
+
+namespace FirstLessonWPFApplication
+{
+    internal static class Format
+    {
+        public static string CoordinatesFormat(string userInput)
+        {
+            var coordinate = CoordinatesHelper.ParseUserInputToCoordinate(userInput);
+            return coordinate != null ? coordinate.ToString() : string.Empty;
+        }
+    }
+}
