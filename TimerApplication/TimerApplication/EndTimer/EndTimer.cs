b356b0f8ce31b05ad98e643a55d05d32c744d62f
Виктория Кокошенко <Viktoryia_Kakoshanka@epam.com>
2019-01-28 07:42:16 +0300

    Refactoring

---
+++ TimerApplication/TimerApplication/EndTimer/EndTimer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TimerApplication
+{
+    public class EndTimer
+    {
+        public event EventHandler<EndTimerEventAgs> EndTimerEventHandler;
+
+        public void OnEndTimer(object sender, EndTimerEventAgs e)
+        {
+            EndTimerEventHandler?.Invoke(sender, e);
+        }
+    }
+}
---
+++ TimerApplication/TimerApplication/EndTimerRecorder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimerApplication.Followers;
+using TimerApplication.ViewModel;
+
+namespace TimerApplication
+{
+    public class EndTimerRecorder
+    {
+        public EndTimer EndTimer { get; }
+
+        public EndTimerRecorder()
+        {
+            var endTimer = new EndTimer();
+            EndTimer = endTimer;
+            new Follower1(endTimer);
+            new Follower2(endTimer);
+            new Follower3(endTimer);
+        }
+    }
+}
