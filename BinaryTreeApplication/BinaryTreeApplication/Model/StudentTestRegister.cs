e321b9b923334b675f27e7ef448b6df6f67a8ab4
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-02-04 18:14:38 +0300

    -

--- BinaryTreeApplication/BinaryTreeApplication/Model/StudentTestRegister.cs
+++ BinaryTreeApplication/BinaryTreeApplication/Model/StudentTestRegister.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BinaryTreeApplication.Model
 {
@@ -10,6 +11,22 @@ namespace BinaryTreeApplication.Model
         public string Student { get; }
         public DateTime Date { get; }
         public int Mark { get; }
+
+        private readonly List<string> _namesStudents = new List<string>
+        {
+            "Tom",
+            "Billy",
+            "Alex",
+            "Mila"
+        };
+
+        private readonly List<string> _namesTests = new List<string>
+        {
+            "Physics",
+            "English",
+            "Literature",
+            "Maths" 
+        };
 
         public StudentTestRegister(string testName, string student, DateTime date, int mark)
         {
@@ -26,6 +43,24 @@ namespace BinaryTreeApplication.Model
                 throw new NullReferenceException();
             }
             return DateTime.Compare(x.Date, y.Date);
+        }
+
+        public StudentTestRegister CreateStudentTestRegisterRundom()
+        {
+            var randomMark = new Random();
+            return new StudentTestRegister(CreateTestRandom(), CreateStudentRandom(), DateTime.Now, randomMark.Next(0, 10));
+        }
+
+        private string CreateStudentRandom()
+        {
+            var random = new Random();
+            return _namesStudents.ElementAt(random.Next(0, _namesStudents.Count));
+        }
+
+        private string CreateTestRandom()
+        {
+            var random = new Random();
+            return _namesTests.ElementAt(random.Next(0, _namesTests.Count));
         }
     }
 }
