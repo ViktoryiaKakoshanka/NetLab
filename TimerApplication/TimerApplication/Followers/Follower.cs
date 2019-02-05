744ae477ac1d8a1e39437235204045615854afd5
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-02-05 12:13:01 +0300

    -

---
+++ TimerApplication/TimerApplication/Followers/Follower.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using TimerApplication.Timer;
+
+namespace TimerApplication.Followers
+{
+    public class Follower1
+    {
+        private readonly string _name;
+
+        public Follower1(string followerName, MyTimer timer)
+        {
+            _name = followerName;
+            timer.EndTimerEventHandler += (sender, e) => 
+                { MessageBox.Show($"User set {e.UserCount} seconds.", _name); };
+        }
+    }
+}
