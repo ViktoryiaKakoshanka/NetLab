da08ddbb75eb1747f1caaa0511c7f58ae270ed09
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-02-06 18:04:59 +0300

    -

---
+++ TimerApplication/TimerApplication/Timer/ITimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimerApplication.Timer
+{
+    public interface ITimer
+    {
+        int InitialNumberSeconds { get; set; }
+        int RestNumberSeconds { get; }
+
+        event EventHandler EndTimerEventHandler;
+        event EventHandler UpdateRestTimeEventHandler;
+
+        void OnEndTimer(object sender);
+        void OnUpdateRestTime(object sender);
+
+        void RunTimer(int durationInMilliseconds = 1000);
+    }
+}
