23097fa290df2aeeefbb43cdf8222c988bd5752a
Viktoryia_Kakoshanka <Viktoryia_Kakoshanka@epam.com>
2019-02-22 10:51:39 +0300

    -

---
+++ BinaryTreeApplication/BinaryTreeApplication/ViewModel/ManagerViewModelHelper.cs
@@ -0,0 +1,31 @@
+﻿using BinaryTreeApplication.Model;
+using System.Collections.ObjectModel;
+
+namespace BinaryTreeApplication.ViewModel
+{
+    public static class ManagerViewModelHelper
+    {
+        public static BinaryTree<Register> GenerateDataTree()
+        {
+            var tree = new BinaryTree<Register>(Register.GenerateNewRegister());
+
+            for (var i = 0; i < 15; i++)
+            {
+                tree.Insert(Register.GenerateNewRegister());
+            }
+
+            return tree;
+        }
+
+        public static ObservableCollection<string> GenerateNamesOfFields()
+        {
+            return new ObservableCollection<string>
+            {
+                nameof(Register.TestName),
+                nameof(Register.Student),
+                nameof(Register.Date),
+                nameof(Register.Mark)
+            };
+        }
+    }
+}
